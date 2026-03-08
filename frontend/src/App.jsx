0a19317a00c9f1500f4569abf7a96eacc0ffb070
marshallborham <mtborham@gmail.com>
2026-03-08 14:28:34 -0700

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
+import { Routes, Route } from "react-router";
+import { VALID_ROUTES } from "../../shared/ValidRoutes.js";import { MainLayout } from "./MainLayout.jsx";
 import { AllImages } from "./images/AllImages.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
 import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <AllImages />,
-        <ImageDetails imageId={"0"} />,
-        <UploadPage />,
-        <LoginPage />
-    ];
-
-    return POSSIBLE_PAGES[0];
+    return (
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route index element={<AllImages />} />
+                <Route path={VALID_ROUTES.UPLOAD} element={<UploadPage />} />
+                <Route path={VALID_ROUTES.LOGIN} element={<LoginPage />} />
+                <Route path={VALID_ROUTES.IMAGE_DETAILS} element={<ImageDetails />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,23 +1,19 @@
 import React from "react";
-import { MainLayout } from "./MainLayout.jsx";
 import "./LoginPage.css";
 
 export function LoginPage() {
     const usernameInputId = React.useId();
     const passwordInputId = React.useId();
-
     return (
-        <MainLayout>
+        <>
             <h2>Login</h2>
             <form className="LoginPage-form">
                 <label htmlFor={usernameInputId}>Username</label>
                 <input id={usernameInputId} required />
-
                 <label htmlFor={passwordInputId}>Password</label>
                 <input id={passwordInputId} type="password" required />
-
                 <input type="submit" value="Submit" />
             </form>
-        </MainLayout>
+        </>
     );
 }
